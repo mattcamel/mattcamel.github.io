597e1f8b80407ee0fa5b4f30a11f58903642a9fa
Matthew Camellato <matthewcamellato@Matthews-MacBook-Air.local>
2019-12-03 16:02:06 -0800

    Updates

---
+++ precache-manifest.2fa55ab7acc4cd08a9d6d1e3e22e5d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505935b4a38f8e34a452c15c4891da51",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f7b18bb1dc5f6735c89b",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "bbaef8c154ec7dc13078",
+    "url": "/react-tutorial/static/js/2.294ca4ba.chunk.js"
+  },
+  {
+    "revision": "f7b18bb1dc5f6735c89b",
+    "url": "/react-tutorial/static/js/main.9772dac1.chunk.js"
+  },
+  {
+    "revision": "d1aedc88150eaef040b0",
+    "url": "/react-tutorial/static/js/runtime-main.599fc667.js"
+  }
+]);
